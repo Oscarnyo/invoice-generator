f6920b9054914f7c13fe4209ba45af2ea869ad4d
Oscarnyo <oscarnyo728@gmail.com>
2022-05-23 16:05:39 +0800

    configure css

---
+++ index.js
@@ -0,0 +1,7 @@
+const washbtn = document.getElementById("wash-btn")
+const mowbtn = document.getElementById("mow-btn")
+const pullbtn = document.getElementById("pull-btn")
+let additem = document.getElementById("additem")
+let addprice = document.getElementById("addprice")
+let totalAmount = document.getElementById("total-amt")
+const resetBtn = document.getElementById("reset-btn")
