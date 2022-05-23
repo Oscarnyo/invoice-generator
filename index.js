03e1d1deb1192fd3d295cb7511563df2d4d5982f
Oscarnyo <oscarnyo728@gmail.com>
2022-05-23 21:03:39 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const washbtn = document.getElementById("wash-btn")
 const mowbtn = document.getElementById("mow-btn")
 const pullbtn = document.getElementById("pull-btn")
-let additem = document.getElementById("additem")
+let additem = document.querySelector(".additem")
 let addprice = document.getElementById("addprice")
 let totalAmount = document.getElementById("total-amt")
 const resetBtn = document.getElementById("reset-btn")
+
